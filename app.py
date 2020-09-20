82e0f9799f5c8e08768bb3f86a24e4d1b3891403
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2020-09-20 16:35:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+from flask import Flask, jsonify, request
+from face_recognition import get_predictions
+import config
+
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+
+def predict():
+    predictions = get_predictions(request)
+    
+    return jsonify(predictions)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=config.PORT,debug=False,threaded=False)
+
