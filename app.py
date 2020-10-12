a83579c34726e38876d13f3fc6023c85b9abbeae
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2020-10-12 12:17:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 from flask import Flask, jsonify, request
 from face_recognition import get_predictions
 from os import environ as env
-
+import config
 
 app = Flask(__name__)
 
@@ -16,5 +16,5 @@ def predict():
     return jsonify(predictions)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=int(env.get("PORT", 8080)),debug=False,threaded=False)
+    app.run(host="0.0.0.0",port=config.PORT,debug=False,threaded=False)
 
